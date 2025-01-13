f46032d7ba9777e817df2638c6408c9027b12700
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-01-13 12:07:18 +0530

    Time: 11 ms (54.97%), Space: 45.8 MB (93.38%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -1,93 +1,16 @@
 class Solution {
-     public static boolean funcLeft(char c,char []ch,int i)
-    {
-        int flag=0;
-        for(int j=i-1;j>=0;j--)
-        {
-            if(ch[j]==c)
-            {
-                flag=1;
-                break;
-            }
-        }
-        if(flag==1)
-        {
-            return true;
-        }
-        return false;
-    }
-    public static boolean funcRight(char c,char []ch,int i)
-    {
-        int flag=0;
-        for(int j=i+1;j<ch.length;j++)
-        {
-            if(ch[j]==c)
-            {
-                // System.out.println(c);
-                flag=1;
-                break;
-            }
-        }
-        if(flag==1)
-        {
-            return true;
-        }
-        return false;
-    }
-     public static int funcLeft1(char c,char []ch,int i)
-    {
-        int flag=0;
-        for(int j=i-1;j>=0;j--)
-        {
-            if(ch[j]==c)
-            {
-                flag=j;
-                break;
-            }
-        }
-        return flag;
-    }
-    public static int funcRight1(char c,char []ch,int i)
-    {
-        int flag=0;
-        for(int j=i+1;j<ch.length;j++)
-        {
-            if(ch[j]==c)
-            {
-                // System.out.println(c);
-                flag=j;
-                break;
-            }
-        }
-        
-        return flag;
-    }
     public int minimumLength(String s) {
-        char ch[]=s.toCharArray();
-        for(int i=1;i<s.length();i++)
+        int freq[]=new int[26];
+        for(char chh:s.toCharArray()) freq[chh-'a']++;
+        int ans=0;
+        for(int i:freq)
         {
-            char c=ch[i];
-            if(funcLeft(c,ch,i) && funcRight(c,ch,i))
+            while(i>=3)
             {
-                    int a=funcRight1(c,ch,i);
-	                 int b=funcLeft1(c,ch,i);
-	                ch[a]=' ';
-	                ch[b]=' ';
+                i-=2;
             }
-            
+            ans+=i;
         }
-        int cnt=0;
-        for(char chh:ch)
-        {
-
-            if(chh==' ')
-            {
-                // cnt++;
-            }else
-            {
-                cnt++;
-            }
-        }
-        return cnt;
+        return ans;
     }
 }
