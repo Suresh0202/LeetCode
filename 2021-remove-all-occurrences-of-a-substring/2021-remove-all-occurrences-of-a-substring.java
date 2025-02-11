37e461ceefc7d9be77e6bbf1814b5e6d84a057d7
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-02-11 18:31:37 +0530

    Time: 187 ms (5.01%), Space: 45.3 MB (5.62%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,68 @@
+class Solution {
+    void func(Stack<Character> st, String part) {
+        int n = part.length() - 1;
+        int n1=part.length()-1;
+        int ans=0;
+        List<Character>aa=new ArrayList<>();
+        while(n>=0 && !st.isEmpty())
+        {
+            if(st.peek()  == part.charAt(n))
+            {
+                ans=n;
+                aa.add(st.pop());
+                // st.pop();
+            }else
+            {
+                break;
+            }
+            n--;
+        }
+          System.out.println("before " + st);
+        for(int i=aa.size()-1;i>=0;i--)
+        {
+            st.add(aa.get(i));
+        }
+        // System.out.println(ans);
+         System.out.println("after " + st);
+        if(ans==0)
+        {
+             while(n1>=0 && !st.isEmpty())
+        {
+            if(st.peek()  == part.charAt(n1))
+            {
+                // ans=n;
+                st.pop();
+            }else
+            {
+                break;
+            }
+            n1--;
+        }
+        
+               
+        }
+        
+
+
+    }
+
+    public String removeOccurrences(String s, String part) {
+        Stack<Character> st = new Stack<>();
+        int n = part.length();
+        for (int i = 0; i < s.length(); i++) {
+            st.push(s.charAt(i));
+            if (st.peek() == part.charAt(n - 1)) {
+                System.out.println("before " + st);
+                func(st, part);
+                System.out.println("after " + st);
+            }
+        }
+        String ss="";
+        for(char ch:st)
+        {
+            ss+=ch;
+        }
+        // System.out.println(st);
+        return ss;
+    }
+}
