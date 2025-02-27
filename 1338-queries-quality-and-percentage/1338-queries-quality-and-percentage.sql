5ef5f9133d07c9a97d188e80f54dc1a216ccfce1
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-02-27 22:05:02 +0530

    Time: 447 ms (88.44%), Space: 0B (100%) - LeetHub

---
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -0,0 +1,9 @@
+select query_name,round(sum((rating/position))/(count(query_name)),2) as quality,
+round(sum(rating < 3)/count(query_name)*100,2)   as poor_query_percentage 
+from Queries
+group by query_name
+
+-- select count(rating) 
+-- from Queries 
+-- where rating <3
+-- group by query_name
