9be86dad1d6f9c65f9448641bd34d1373a4c9eab
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-02-28 00:26:14 +0530

    Time: 31 ms (66.94%), Space: 58.1 MB (90.98%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,29 +1,25 @@
 class Solution {
     public int[] maxSlidingWindow(int[] arr, int k) {
         int n=arr.length;
-       int ans[]=new int[n-k+1];
-       TreeMap<Integer,Integer>map=new TreeMap<>();
-       int i=0;
-       int j=0;
-       while(j<n)
-       {
-        map.put(arr[j],map.getOrDefault(arr[j],0)+1);
-        if((j-i+1)>=k)
-        {
-            ans[i]=map.lastKey();
-            if(map.get(arr[i])>1)
+            int ans[]=new int[n-k+1];
+            int ind=0;
+            Deque<Integer>qu=new LinkedList<>();
+            for(int i=0;i<n;i++)
             {
-                map.put(arr[i],map.get(arr[i])-1);
-            }else
-            {
-                map.remove(arr[i]);
+                while(!qu.isEmpty() && qu.peekFirst()< i-k+1)
+                {
+                    qu.pollFirst();
+                }
+                while(!qu.isEmpty() && arr[qu.peekLast()]<arr[i])
+                {
+                    qu.pollLast();
+                }
+                qu.offerLast(i);
+                if(i>=(k-1))
+                {
+                    ans[ind++]=arr[qu.peekFirst()];
+                }
             }
-            i++;
-        }
-        j++;
-       }
-       return ans;
-
-        // return ans;
+            return ans;
     }
 }
