50d10478d48c74c0a3a0e37105d7bc236cbcdc67
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-02-28 23:19:58 +0530

    Time: 79 ms (16.78%), Space: 45.1 MB (16.42%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,23 +1,36 @@
 class Solution {
-    boolean func(StringBuilder sb,char ch2[])
+    boolean func(StringBuilder sb,int ch2[])
     {
-        char ch1[]=sb.toString().toCharArray();
-       
-        Arrays.sort(ch1);
+        int ch1[]=new int [26];
+        String s=sb.toString();
+        for(int i=0;i<sb.length();i++)
+        {
+                ch1[s.charAt(i)-'a']++;
+        }
         // Sys/tem.out.println(Arrays.toString(ch1));
         // System.out.println(Arrays.toString(ch2));
-        if(Arrays.equals(ch1,ch2))
+       for(int i=0;i<26;i++)
+       {
+        if(ch1[i]!=ch2[i])
         {
-            return true;
+            return false;
         }
-        return false;
+        
+       }
+       return true;
     }
     public boolean checkInclusion(String s1, String s2) {
         List<String>ar=new ArrayList<>();
         int r=0,l=0;
         int n=s2.length();
-         char ch2[]=s1.toCharArray();
-         Arrays.sort(ch2);
+        //  char ch2[]=s1.toCharArray();
+        //  Arrays.sort(ch2);
+        int ch2[]=new int [26];
+        // String s=sb.toString();
+        for(int i=0;i<s1.length();i++)
+        {
+                ch2[s1.charAt(i)-'a']++;
+        }
         StringBuilder ss=new StringBuilder();
         while(r<n)
         {
