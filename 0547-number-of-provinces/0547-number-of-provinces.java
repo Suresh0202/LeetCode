b8cfb5effe187abf10bfee1181cd84a1b1ed8dd3
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-03-03 01:36:59 +0530

    Time: 12 ms (5.73%), Space: 45.6 MB (94.43%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void bfs(int vis[],List<List<Integer>>ajd,int node)
+    {
+        vis[node]=1;
+        for(int i:ajd.get(node))
+        {
+            if(vis[i]==0)
+            {
+                bfs(vis,ajd,i);
+            }
+        }
+    }
+    public int findCircleNum(int[][] adj) {
+        List<List<Integer>>adjlist=new ArrayList<>();
+        for(int i=0;i<adj.length;i++)
+        {
+            adjlist.add(new ArrayList<>());
+        }
+        for(int i=0;i<adj.length;i++)
+        {
+            for(int j=0;j<adj.length;j++)
+            {
+                if( adj[i][j] == 1 & i!=j)
+                {
+                    adjlist.get(i).add(j);
+                    adjlist.get(j).add(j);
+                }
+            }
+        }
+        System.out.println(adjlist);
+        int vis[]=new int[adjlist.size()];
+        int c=0;
+        for(int i=0;i<adj.length;i++)
+        {
+            if(vis[i] == 0)
+            {
+                c++;
+                bfs(vis,adjlist,i);
+            }
+        }
+        return c;
+    }
+}
