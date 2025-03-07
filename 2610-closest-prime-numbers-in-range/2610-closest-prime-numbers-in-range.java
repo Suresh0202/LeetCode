8268a0e108c3f9e86cf8a5a890e360afd4654189
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-03-07 10:27:54 +0530

    Time: 118 ms (33.2%), Space: 56.7 MB (6.4%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -24,30 +24,19 @@ class Solution {
                 ar.add(i);
             }
         }
-        //  System.out.println(ar);
         int m=Integer.MAX_VALUE;
         ans[0]=-1;
         ans[1]=-1;
         for(int i=1;i<ar.size();i++)
         {
-            int x=ar.get(i);
-            int y=ar.get(i-1);
-            int d=x-y;
+            int d=ar.get(i)-ar.get(i-1);
             if(d<m)
             {
                 m=d;
-                // ans1=Math.min(ans1,x);
-                // ans2=Math.min(ans2,y);
-                ans[0]=y;
-                ans[1]=x;
+                ans[0]=ar.get(i-1);
+                ans[1]=ar.get(i);
             }
 
-        }
-        // ans[0]=ans1;
-        // ans[1]=ans2;
-        if(ans[0]==-1 && ans[1]== -1)
-        {
-            return new int[]{-1,-1};
         }
         return ans;
     }
