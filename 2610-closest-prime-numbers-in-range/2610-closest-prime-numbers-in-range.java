f47c3ddd3877fcda12bc8c1e860ccb12d5728c23
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-03-07 18:52:05 +0530

    Time: 1750 ms (5.2%), Space: 54.8 MB (20.4%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -1,42 +1,43 @@
 class Solution {
-    public int[] closestPrimes(int left, int right) {
-        int prime[]=new int[right+1];
-        Arrays.fill(prime,1);
-        prime[0]=0;
-        prime[1]=0;
-        int n=right;
-        for(int i=2;(i*i)<=n;i++)
-        {
-            if(prime[i]==1)
-            {
-            for(int j=(i*i);j<=n;j+=i)
-            {
-                prime[j]=0;
-            }
+    public boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
             }
         }
-        int ans[]=new int[2];
-        List<Integer>ar=new ArrayList<>();
-        for(int i=left;i<=right;i++)
-        {
-            if(prime[i]==1)
-            {
+        return true;
+    }
+
+    public int[] closestPrimes(int left, int right) {
+        List<Integer> ar = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            if (isPrime(i)) {
                 ar.add(i);
             }
         }
-        int m=Integer.MAX_VALUE;
+        System.out.println(ar);
+        int ans[] = new int[2];
+        int m = Integer.MAX_VALUE;
         ans[0]=-1;
         ans[1]=-1;
-        for(int i=1;i<ar.size();i++)
-        {
-            int d=ar.get(i)-ar.get(i-1);
-            if(d<m)
-            {
-                m=d;
-                ans[0]=ar.get(i-1);
-                ans[1]=ar.get(i);
+        for (int i = 1; i < ar.size(); i++) {
+            int x = ar.get(i);
+            int y = ar.get(i - 1);
+            int d = x - y;
+            if (d < m) {
+                m = d;
+                // ans1=Math.min(ans1,x);
+                // ans2=Math.min(ans2,y);
+                ans[0] = y;
+                ans[1] = x;
             }
 
+        }
+
+        if (ans[0] == -1 && ans[1] == -1) {
+            return new int[] { -1, -1 };
         }
         return ans;
     }
