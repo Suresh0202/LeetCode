798bf679606caf998b80de0df47c649a4f02bb64
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-03-10 20:28:40 +0530

    Time: 8 ms (76.65%), Space: 54.4 MB (76.31%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -1,83 +1,60 @@
-class Solution {
-     public void dfs(int [][]image,int sr,int sc,int old,int ns,int arr[][])
+class Pair{
+    int first;
+    int second;
+    Pair(int first,int second)
     {
-        if(sc<0 || sr<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]==ns || arr[sr][sc]==-1)
-        {
-            return;
-        }
-        
-        if (image[sr][sc] == old) {
-            arr[sr][sc]=-1;
-            image[sr][sc] = ns;
-        dfs(image,sr-1,sc,old,ns,arr);
-        dfs(image,sr,sc+1,old,ns,arr);
-        dfs(image,sr+1,sc,old,ns,arr);
-        dfs(image,sr,sc-1,old,ns,arr);
-        }
+        this.first=first;
+        this.second=second;
     }
-    public int numEnclaves(int[][] board) {
-       int n=board.length;
-        int m=board[0].length;
-        int arr[][]=new int[n][m];
+}
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        Queue<Pair>qu=new LinkedList<Pair>();
+        int n=grid.length;
+        int m=grid[0].length;
+        int vis[][]=new int[n][m];
         for(int i=0;i<n;i++)
         {
-            Arrays.fill(arr[i],2);
-        }
-        for(int j=0;j<m;j++)
-        {
-            if(board[0][j]==1)
+            for(int j=0;j<m;j++)
             {
-                  int sr=0,sc=j;
-                   dfs(board,sr,sc,1,0,arr);
-            }else
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    // add all the corner 1s into queue
+                    if(grid[i][j]==1)
+                    qu.add(new Pair(i,j));
+                    vis[i][j]=1;
+                }
+            }
+        }
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+        while(!(qu.isEmpty()))
         {
-            arr[0][j]=-1;
+            int row=qu.peek().first;
+            int col=qu.peek().second;
+            qu.remove();
+            for(int i=0;i<4;i++)
+            {
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1)
+                {
+                    qu.add(new Pair(nrow,ncol));
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(vis[i][j]==0 && grid[i][j]==1)
+                {
+                    ans++;
+                }
+            }
         }
 
-        }
-        for(int i=1;i<n;i++)
-        {
-            if(board[i][m-1]==1)
-            {
-                int sr=i,sc=m-1;
-                   dfs(board,sr,sc,1,0,arr);
-            }else
-        {
-            arr[i][m-1]=-1;
-        }
-        }
-       for(int j=0;j<m;j++) 
-       {
-        if(board[n-1][j]==1)
-        {
-            int sr=n-1,sc=j;
-                   dfs(board,sr,sc,1,0,arr);
-        }else
-        {
-            arr[n-1][j]=-1;
-        }
-       }
-       for(int i=0;i<n;i++) 
-       {
-        if(board[i][0]==1)
-        {
-             int sr=i,sc=0;
-            dfs(board,sr,sc,1,0,arr);
-        }else
-        {
-            arr[i][0]=-1;
-        }
-       }
-       int ans=0;
-       for(int i=1;i<n-1;i++)
-       {
-        for(int j=1;j<m-1;j++)
-        {
-        //    System.out.print(arr[i])
-        if(board[i][j]==1 && arr[i][j]==2)
-           ans++;
-        }
-       }
-       return ans;
+return ans;
     }
 }
