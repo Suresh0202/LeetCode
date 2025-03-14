e646f1661f463a0dc7403fbcdd42df5b1e2db99a
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-03-14 14:06:20 +0530

    Time: 13 ms (29.91%), Space: 49.9 MB (73.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -6,11 +6,11 @@ class Solution {
             mp.put(i,mp.getOrDefault(i,0)+1);
 
         }
-        System.out.println(mp);
+        // System.out.println(mp);
         int k=nums.length/2;
         for(Map.Entry<Integer,Integer>m:mp.entrySet())
         {
-            System.out.println(m.getKey()+" "+m.getValue());
+            // System.out.println(m.getKey()+" "+m.getValue());
             int key=m.getValue();
             if(key > k)
             {
