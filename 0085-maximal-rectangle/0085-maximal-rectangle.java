25c4f4992536d160296e1a3eb39d9f3fe173a1c2
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-04-09 20:19:48 +0530

    Time: 28 ms (11.64%), Space: 45.4 MB (89.16%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,72 @@
+class Solution {
+    public int histogram(int hh[])
+    {
+        int n=hh.length;
+        int nse[]=new int[n];
+        Stack<Integer>st=new Stack<>();
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.isEmpty() && hh[st.peek()]>=hh[i])
+            {
+                st.pop();
+            }
+            if(!st.isEmpty())
+            {
+                nse[i]=st.peek();
+            }else
+            {
+                nse[i]=n;
+            }
+            st.push(i);
+        }
+        int lnse[]=new int[n];
+        Stack<Integer>st1=new Stack<>();
+        for(int i=0;i<n;i++)
+        {
+            while(!st1.isEmpty() && hh[st1.peek()]>=hh[i])
+            {
+                st1.pop();
+            }
+            if(!st1.isEmpty())
+            {
+                lnse[i]=st1.peek();
+            }else
+            {
+                lnse[i]=-1;
+            }
+            st1.push(i);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int x=(nse[i]-lnse[i]-1)*hh[i];
+            ans=Math.max(ans,x);
+        }
+        // System.out.println(Arrays.toString(lnse));
+        return ans;
+    }
+    public int maximalRectangle(char[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int mat[][]=new int[n][m];
+        for(int j=0;j<m;j++)
+        {
+            int pre=0;
+            for(int i=0;i<n;i++)
+            {
+                if(matrix[i][j]=='0')
+                {
+                    pre=0;
+                }
+                pre+=matrix[i][j]-'0';
+                mat[i][j]=pre;
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans=Math.max(ans,histogram(mat[i]));
+        }
+        return ans;
+    }
+}
