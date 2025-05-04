ecb586e349289a308f8489836a2e542d565e3c96
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-05-04 20:21:27 +0530

    Time: 155 ms (43.53%), Space: 41.7 MB (63.68%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,42 +1,25 @@
 class Solution {
-    public boolean dfs(char board[][],int sr,int sc,String word,char vis[][],int idx)
-    {
-        if(idx == word.length())
-        {
-            return true;
-        }
-        if(sr<0 || sc<0 || sr>=board.length || sc>=board[0].length || board[sr][sc]!=word.charAt(idx))
-        {
-            return false;
-        }
-        char temp=board[sr][sc];
-         board[sr][sc]=')';  
-          boolean ans=  
-          dfs(board,sr+1,sc,word,vis,idx+1)||
-          dfs(board,sr-1,sc,word,vis,idx+1)||
-          dfs(board,sr,sc+1,word,vis,idx+1)||
-          dfs(board,sr,sc-1,word,vis,idx+1);
-
-    board[sr][sc]=temp;
-          return ans;
-        
-        
-    }
     public boolean exist(char[][] board, String word) {
-        int n=board.length;
-        int m=board[0].length;
-        char vis[][]=new char[n][m];
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(board[i][j] ==word.charAt(0) && dfs(board,i,j,word,vis,0))
-                {
-                    return true;
-                }
-
+        int rows = board.length, cols = board[0].length;
+        int vis[][] = new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(dfs(board, i, j, rows, cols, vis, 0, word)) return true;
             }
         }
+        return false;
+    }
+    boolean dfs(char[][] board, int i, int j, int rows, int cols, int[][] vis, int idx, String word){
+        if(idx == word.length()) return true;
+        if(i == rows || j == cols || i<0 || j <0 || vis[i][j] == 1 || board[i][j] != word.charAt(idx)){
+            return false;
+        }
+        vis[i][j] = 1;
+        if(dfs(board, i+1, j, rows, cols, vis, idx+1, word)) return true;
+        if(dfs(board, i, j+1, rows, cols, vis, idx+1, word)) return true;
+        if(dfs(board, i-1, j, rows, cols, vis, idx+1, word)) return true;
+        if(dfs(board, i, j-1, rows, cols, vis, idx+1, word)) return true;
+        vis[i][j] = 0;
         return false;
     }
 }
