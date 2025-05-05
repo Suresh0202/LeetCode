e5a42f81613000f54cee29d9ce46c65800e31b6f
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-05-05 18:29:20 +0530

    Time: 153 ms (45.45%), Space: 41.3 MB (98.1%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,25 +1,53 @@
 class Solution {
-    public boolean exist(char[][] board, String word) {
-        int rows = board.length, cols = board[0].length;
-        int vis[][] = new int[rows][cols];
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(dfs(board, i, j, rows, cols, vis, 0, word)) return true;
-            }
+    public boolean dfs(char board[][],int vis[][],int sr,int sc,String word,int idx)
+    {
+        if(idx == word.length())
+        {
+            return true;
         }
-        return false;
-    }
-    boolean dfs(char[][] board, int i, int j, int rows, int cols, int[][] vis, int idx, String word){
-        if(idx == word.length()) return true;
-        if(i == rows || j == cols || i<0 || j <0 || vis[i][j] == 1 || board[i][j] != word.charAt(idx)){
+        if(sr<0 || sc<0 || sr>=board.length || sc>=board[0].length || vis[sr][sc] == 1 || board[sr][sc]!=word.charAt(idx))
+        {
             return false;
         }
-        vis[i][j] = 1;
-        if(dfs(board, i+1, j, rows, cols, vis, idx+1, word)) return true;
-        if(dfs(board, i, j+1, rows, cols, vis, idx+1, word)) return true;
-        if(dfs(board, i-1, j, rows, cols, vis, idx+1, word)) return true;
-        if(dfs(board, i, j-1, rows, cols, vis, idx+1, word)) return true;
-        vis[i][j] = 0;
+        vis[sr][sc]=1;
+        if(dfs(board,vis,sr+1,sc,word,idx+1)){
+            return true;
+        }
+        if(dfs(board,vis,sr-1,sc,word,idx+1))
+        {
+            return true;
+        }
+        if(dfs(board,vis,sr,sc+1,word,idx+1))
+        {
+            return true;
+        }
+        if(dfs(board,vis,sr,sc-1,word,idx+1))
+        {
+            return true;
+        }
+        vis[sr][sc]=0;
+        return false;
+    }
+    public boolean exist(char[][] board, String word) {
+        int n=board.length;
+        int m=board[0].length;
+        int vis[][]=new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(board[i][j] == word.charAt(0) && vis[i][j]==0)
+                {
+                     if(dfs(board,vis,i,j,word,0)){
+                        return true;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(Arrays.toString(vis[i]));
+        }
         return false;
     }
 }
