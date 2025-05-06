45726b9ac12e83b7fc3248f14571fd071823fda3
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-05-06 10:11:51 +0530

    Time: 0 ms (100%), Space: 46.4 MB (50.32%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,43 +1,25 @@
 class Solution {
-    public static int[] prefixmax(int ar[])
-    {
-        int pre[]=new int[ar.length];
-        pre[0]=ar[0];
-        for(int i=1;i<ar.length;i++)
-        {
-                pre[i]=Math.max(pre[i-1],ar[i]);
-        }
-        return pre;
-    }
-     public static int[] suffixmax(int ar[])
-    {
-        int pre[]=new int[ar.length];
-        pre[ar.length-1]=ar[ar.length-1];
-        for(int i=ar.length-2;i>=0;i--)
-        {
-                pre[i]=Math.max(pre[i+1],ar[i]);
-        }
-        return pre;
-    }
-    public static int findans(int pre[],int suf[],int h[])
-    {
+    public int trap(int[] h) {
+        int left=0;
+        int right=h.length-1;
         int ans=0;
-        for(int i=0;i<h.length-1;i++)
+        int lef=0;
+        int righ=0;
+        while(left < right)
         {
-            int lf=pre[i];
-            int rm=suf[i];
-            if(lf > h[i] && rm>h[i])
+            if(h[left] < h[right])
             {
-                ans+=Math.min(lf,rm)-h[i];
+                lef=Math.max(lef,h[left]);
+
+                ans+=lef-h[left];
+                left++;
+            }else
+            { righ=Math.max(righ,h[right]);
+
+                ans+=righ-h[right];
+                right--;
             }
         }
-        return ans;
-    }
-
-    public int trap(int[] height) {
-        int pref[]=prefixmax(height);
-        int suff[]=suffixmax(height);
-        int ans=findans(pref,suff,height);
         return ans;
     }
 }
