04139a392dcfd75de2bc949b965616de5ee34472
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-05-15 15:11:35 +0530

    Time: 0 ms (100%), Space: 40.5 MB (48.33%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,28 +1,28 @@
 class Solution {
-    public int func(int i,int j,int n,int m,int dp[][])
-    {
-        if(i>n || j>m)
-        {
-            return 0;
-        }
-        if(i == n && j == m)
-        {
-            return 1;
-        }
-        if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-        int left=func(i+1,j,n,m,dp);
-        int right=func(i,j+1,n,m,dp);
-        return dp[i][j]=left+right;
-    }
     public int uniquePaths(int m, int n) {
         int dp[][]=new int[m+1][n+1];
+        dp[0][0]=1;
         for(int i=0;i<m;i++)
         {
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j == 0)
+                {
+
+                }else
+                {
+                    int up=0,dw=0;
+                    if(i>0)
+                    {
+                        up=dp[i-1][j];
+                    }
+                    if(j>0){
+                        dw=dp[i][j-1];
+                    }
+                    dp[i][j]=up+dw;
+                }
+            }
         }
-        return func(0,0,m-1,n-1,dp);
+        return dp[m-1][n-1];
     }
 }
