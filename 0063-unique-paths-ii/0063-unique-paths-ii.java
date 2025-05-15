4f64d58e9e86c559f210d300f2ab9a36aeae4352
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-05-15 15:28:27 +0530

    Time: 0 ms (100%), Space: 42.1 MB (19.56%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,29 +1,36 @@
 class Solution {
-    public int func(int i,int j,int dp[][],int arr[][])
-    {
-        if(i<0 || j<0 || arr[i][j] == 1){
-            return 0;
-        }
-        if(i ==0 && j == 0)
-        {
-            return 1;
-        }
-        if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-        int left=func(i-1,j,dp,arr);
-        int right=func(i,j-1,dp,arr);
-        return dp[i][j]=left+right;
-    }
     public int uniquePathsWithObstacles(int[][] obs) {
-        int n=obs.length;
-        int m=obs[0].length;
-        int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++)
-        {
-            Arrays.fill(dp[i],-1);
-        }
-        return func(n-1,m-1,dp,obs);
+            int n=obs.length;
+            int m=obs[0].length;
+            int dp[][]=new int[n][m];
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<m;j++)
+                {
+                     if(obs[i][j] == 1)
+                    {
+                        dp[i][j]=0;
+                        // continue;
+                    }
+                  else  if(i == 0 && j == 0)
+                    {
+                        dp[i][j]=1;
+
+                    }
+                    else{
+                    int up=0,dw=0;
+                    if(i>0)
+                    {
+                        up=dp[i-1][j];
+                    }
+                    if(j>0)
+                    {
+                        dw=dp[i][j-1];
+                    }
+                    dp[i][j]=up+dw;
+                    }
+                }
+            }
+            return dp[n-1][m-1];
     }
 }
