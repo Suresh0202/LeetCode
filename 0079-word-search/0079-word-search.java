f190587e86a3a7fd98cdd66c282bd5eff583fc67
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-07-02 22:58:50 +0530

    Time: 146 ms (51.46%), Space: 42.1 MB (23.82%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,52 +1,51 @@
 class Solution {
-    public boolean dfs(char board[][],int vis[][],int sr,int sc,String word,int idx)
+    public boolean dfs(int sr,int sc,char board[][],int vis[][],String word,int idx)
     {
         if(idx == word.length())
         {
             return true;
         }
-        if(sr<0 || sc<0 || sr>=board.length || sc>=board[0].length || vis[sr][sc] == 1 || board[sr][sc]!=word.charAt(idx))
+        if(sr<0 || sc<0 || sr>=board.length || sc>=board[0].length || board[sr][sc] != word.charAt(idx) || vis[sr][sc] == 1)
         {
             return false;
         }
         vis[sr][sc]=1;
-        if(dfs(board,vis,sr+1,sc,word,idx+1)){
-            return true;
-        }
-        if(dfs(board,vis,sr-1,sc,word,idx+1))
+        if(dfs(sr,sc+1,board,vis,word,idx+1))
         {
             return true;
         }
-        if(dfs(board,vis,sr,sc+1,word,idx+1))
+        if(dfs(sr,sc-1,board,vis,word,idx+1))
         {
             return true;
         }
-        if(dfs(board,vis,sr,sc-1,word,idx+1))
+        if(dfs(sr-1,sc,board,vis,word,idx+1))
+        {
+            return true;
+        }
+        if(dfs(sr+1,sc,board,vis,word,idx+1))
         {
             return true;
         }
         vis[sr][sc]=0;
         return false;
+
     }
     public boolean exist(char[][] board, String word) {
         int n=board.length;
         int m=board[0].length;
-        int vis[][]=new int[n][m];
+        int vis[][]=new int[board.length][board[0].length];
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(board[i][j] == word.charAt(0) && vis[i][j]==0)
+                if(board[i][j] == word.charAt(0) && vis[i][j] == 0)
                 {
-                     if(dfs(board,vis,i,j,word,0)){
+                    if(dfs(i,j,board,vis,word,0))
+                    {
                         return true;
                     }
                 }
             }
-        }
-        for(int i=0;i<n;i++)
-        {
-            System.out.println(Arrays.toString(vis[i]));
         }
         return false;
     }
