2ee1f91ed309e5d3f367b3173c6f0c32d06da70c
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-07-16 09:38:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maximumLength(int[] nums) {
+        int ce=0,co=0,cal=0,vis=-1;
+        for(int i:nums)
+        {
+            if(i%2==1)
+            {
+                co++;
+                if(vis!=1)
+                {
+                    cal++;
+                    vis=1;
+                }
+            }else
+            {
+                ce++;
+                if(vis!=0)
+                {
+                    cal++;
+                    vis=0;
+                }
+            }
+        }
+        if(ce >co && ce> cal)
+        {
+            return ce;
+        }else if(co>ce && co>cal)
+        {
+           return co;
+        }else {
+            return cal;
+        }
+    }
+}
