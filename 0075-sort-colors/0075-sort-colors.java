42d3ea06aff0ceea5cd2a9db593e8f83bf3b1305
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-07-31 00:38:15 +0530

    Time: 0 ms (100%), Space: 41.9 MB (82.74%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void qs(int nums[],int low,int high)
+    {
+        if(low<high)
+        {
+            int part=find(nums,low,high);
+            qs(nums,low,part-1);
+            qs(nums,part+1,high);
+        }
+    }
+    public int find(int nums[],int low,int high)
+    {
+        int pivot=nums[low];
+        int i=low;
+        int j=high;
+        while(i<j)
+        {
+            while(nums[i]<=pivot && i<=high-1)
+            {
+                i++;
+            }
+            while(nums[j]>pivot && j>=low-1)
+            {
+                j--;
+            }
+            if(i<j)
+            {
+                int tp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=tp;
+            }
+        }
+        int tp=nums[j];
+        nums[j]=nums[low];
+        nums[low]=tp;
+        return j;
+
+    }
+    public void sortColors(int[] nums) {
+        qs(nums,0,nums.length-1);
+    }
+}
