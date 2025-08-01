64089dca4be4fdcff2c07f93396180d2fb9fef52
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-08-01 21:05:00 +0530

    Time: 0 ms (100%), Space: 46 MB (22.91%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,9 +1,25 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        if(Arrays.binarySearch(nums,target) < 0)
+    public int func(int nums[],int low,int high,int x)
+    {
+        int mid=(low+high)/2;
+        if(low>high)
         {
             return -1;
         }
-        return  Arrays.binarySearch(nums, target);
+        if(nums[mid] == x)
+        {
+            return mid;
+        }
+        if(nums[mid] < x)
+        {
+            return func(nums,mid+1,high,x);
+        }else if(nums[mid] > x)
+        {
+            return func(nums,low,mid-1,x);
+        }
+        return -1;
+    }
+    public int search(int[] nums, int target) {
+        return func(nums,0,nums.length-1,target);
     }
 }
