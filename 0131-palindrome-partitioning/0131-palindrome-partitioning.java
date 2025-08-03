e557ab964e66567cfaf9eec75c19afb36acb618f
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-08-03 19:56:51 +0530

    Time: 12 ms (10.32%), Space: 56.7 MB (75.9%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,39 +1,39 @@
 class Solution {
-    void func(int idx,List<List<String>>ans ,List<String>path,String s)
-    {
-        if(idx == s.length())
-        {
-            ans.add(new ArrayList<>(path));
-            return;
-        }
-        for(int i=idx;i<s.length();i++)
-        {
-            if(isPalin(s.substring(idx,i+1)))
-            {
-                path.add(s.substring(idx,i+1));
-                func(i+1,ans,path,s);
-                path.remove(path.size()-1);
-            }
-        }
-    }
-    boolean isPalin(String s)
+    public boolean isPalindrome(String s)
     {
         int i=0;
         int j=s.length()-1;
-        while(i<j)
+        while(i<=j)
         {
             if(s.charAt(i) != s.charAt(j)){
                 return false;
             }
             i++;
             j--;
-        }
+        }   
         return true;
+    }
+    public void solve(String s,int idx,int n,List<String>path,List<List<String>>ans)
+    {
+        if(idx == n)
+        {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i=idx;i<n;i++)
+        {
+            if(isPalindrome(s.substring(idx,i+1)))
+            {
+                path.add(s.substring(idx,i+1));
+                solve(s,i+1,n,path,ans);
+                path.remove(path.size()-1);
+            }
+        }
     }
     public List<List<String>> partition(String s) {
         List<List<String>>ans=new ArrayList<>();
         List<String>path=new ArrayList<>();
-        func(0,ans,path,s);
+        solve(s,0,s.length(),path,ans);
         return ans;
     }
 }
