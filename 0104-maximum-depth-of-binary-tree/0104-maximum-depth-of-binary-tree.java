3f54a1f15d1331285d36d39ddf771d1f0cb122aa
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-08-06 00:23:07 +0530

    Time: 0 ms (100%), Space: 42.6 MB (77.13%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,20 +14,13 @@
  * }
  */
 class Solution {
-    public int func(TreeNode root,int ans[])
-    {
+    public int maxDepth(TreeNode root) {
         if(root == null)
         {
             return 0;
         }
-        int left=func(root.left,ans);
-        int right=func(root.right,ans);
-        ans[0]=Math.max(left,right);
+        int left=maxDepth(root.left);
+        int right=maxDepth(root.right);
         return 1+Math.max(left,right);
-    }
-    public int maxDepth(TreeNode root) {
-       int ans[]={0};
-       int k=func(root,ans);
-       return k;
     }
 }
