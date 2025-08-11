5534a7ad1e1502072a78f4a99ecbc1884e0267ba
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-08-11 13:53:31 +0530

    Time: 5 ms (100%), Space: 45.7 MB (52.37%) - LeetHub

---
+++ 0838-design-linked-list/0838-design-linked-list.java
@@ -0,0 +1,45 @@
+class MyLinkedList {
+    List<Integer>arr;
+    public MyLinkedList() {
+        arr=new ArrayList<>();
+    }
+    
+    public int get(int id) {
+        if(id >= 0 && id < arr.size())
+        {
+            return arr.get(id);
+        }
+        return -1;
+    }
+    
+    public void addAtHead(int val) {
+        arr.add(0,val);
+    }
+    
+    public void addAtTail(int val) {
+        arr.add(val);
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if (index >= 0 && index <= arr.size()) {
+            arr.add(index, val);
+        }
+    }
+    
+    public void deleteAtIndex(int index) {
+        if(index>=0 && index < arr.size())
+        {
+        arr.remove(index);
+        }
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
