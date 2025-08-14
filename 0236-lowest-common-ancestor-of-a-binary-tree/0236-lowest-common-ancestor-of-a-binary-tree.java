dd143ca67ed7f42cc5705d59a455b47442af4b96
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-08-14 14:25:02 +0530

    Time: 8 ms (18.06%), Space: 44.9 MB (46.84%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,77 +8,23 @@
  * }
  */
 class Solution {
-    public void func(TreeNode root,TreeNode p,TreeNode q,List<Integer>a,List<List<Integer>>ans)
-    {
-        if(root == null)
-        {
-            return;
-        }
-        if(root == p)
-        {
-            a.add(root.val);
-            ans.add(new ArrayList<>(a));
-            a.remove(a.size()-1);
-            // return ;
-        }
-        if(root == q)
-        {
-            a.add(root.val);
-            ans.add(new ArrayList<>(a));
-            a.remove(a.size()-1);
-            // return;
-        }
-        if(root.left != null)
-        {
-        a.add(root.val);
-        func(root.left,p,q,a,ans);
-        a.remove(a.size()-1);
-
-        }
-        if(root . right != null)
-        {
-          
-
-        a.add(root.val);
-        func(root.right,p,q,a,ans);
-        a.remove(a.size()-1);
-
-        }
-    }
+    // public TreeNode func(TreeNode root,TreeNod)
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<List<Integer>>ans=new ArrayList<>();
-        List<Integer>a=new ArrayList<>();
-        func(root,p,q,a,ans);
-        System.out.println(ans);
-        List<Integer>p1=ans.get(0);
-        List<Integer>p2=ans.get(1);
-        int i=0;
-        int j=0;
-        int f=0;
-        while(i<p1.size() && j<p2.size())
+        // return func(root,p,q);
+        if(root == null || root == p || root == q)
         {
-            i++;
-            j++;
-            if(i<p1.size() && j<p2.size() &&!p1.get(i).equals(p2.get(j)))
-            {
-            //   /
-                break;
-            }
+            return root;
         }
-        if(i == j)
+        TreeNode l=lowestCommonAncestor(root.left,p,q);
+        TreeNode r=lowestCommonAncestor(root.right,p,q);
+        if(l == null)
         {
-            return new TreeNode(p1.get(i-1));
+            return r;
         }
-        if(i == p1.size()-1)
+        else if(r == null)
         {
-            return new TreeNode(p2.get(j-1));
+            return l;
         }
-        if(j == p2.size()-1)
-        {
-            return new TreeNode(p1.get(i-1));
-        }
-        return null;
-        
-        
+        return root;
     }
 }
