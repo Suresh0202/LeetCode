cff4f45bf137c916c5f2498e0805da3daaf2ff74
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-09-05 00:50:18 +0530

    Time: 45 ms (7.69%), Space: 52.7 MB (7.26%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
@@ -1,28 +1,62 @@
 class Solution {
-    public int maxSubarraySumCircular(int[] arr) {
-        int n = arr.length;
-        
-        int total = arr[0];
-        int currMax = arr[0], maxSum = arr[0];
-        int currMin = arr[0], minSum = arr[0];
-        
-        for (int i = 1; i < n; i++) {  // \U0001f448 start from index 1
-            int x = arr[i];
-            total += x;
-            
-            // Kadane for max subarray
-            currMax = Math.max(x, currMax + x);
-            maxSum = Math.max(maxSum, currMax);
-            
-            // Kadane for min subarray
-            currMin = Math.min(x, currMin + x);
-            minSum = Math.min(minSum, currMin);
+     public static int kadenes(int arr[])
+    {
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+      for(int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+           
+            if(sum>max)
+            {
+                max=sum;
+            }
+             if(sum<0)
+            {
+                sum=0;
+            }
+            // max=Math.max(max,sum);
         }
-        
-        // If all numbers are negative → return the largest (maxSum)
-        if (maxSum < 0) return maxSum;
-        
-        // Otherwise, either normal max or circular max
-        return Math.max(maxSum, total - minSum);
+        return max;
+    }
+    public int maxSubarraySumCircular(int[] arr) {
+        int n=arr.length;
+         int normalmax=kadenes(arr);
+            int pre[]=new int[n];
+            int suf[]=new int[n];
+            pre[0]=arr[0];
+            int sum=0;
+            for(int i=1;i<n;i++)
+            {
+                pre[i]=pre[i-1]+arr[i];
+            }
+            suf[n-1]=arr[n-1];
+            
+            for(int i=n-2;i>=0;i--)
+            {
+                suf[i]=suf[i+1]+arr[i];
+            }
+            int max=Integer.MIN_VALUE;
+            int premax[]=new int[n];
+            int sufmax[]=new int[n];
+            premax[0]=pre[0];
+            for(int i=1;i<n;i++)
+            {
+                premax[i]=Math.max(pre[i],premax[i-1]);
+            }
+            sufmax[n-1]=suf[n-1];
+             for(int i=n-2;i>=0;i--)
+            {
+                sufmax[i]=Math.max(suf[i],sufmax[i+1]);
+            }
+            System.out.println(Arrays.toString(sufmax));
+              System.out.println(Arrays.toString(premax));
+            
+            for(int i=0;i<n-1;i++)
+            {
+                int sm=premax[i]+sufmax[i+1];
+                max=Math.max(max,sm);
+            }
+            return Math.max(max,normalmax);
     }
 }
