6579470b1bafe2f6981bb7cb203f955f4eb22dd3
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-09-04 22:00:55 +0530

    Time: 8 ms (78.52%), Space: 45.1 MB (76.56%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,35 +1,36 @@
 class Solution {
-    public static int findmax(int arr[])
-    {
-        int m=-1;
-        for(int i:arr)
-        {
-            m=Math.max(m,i);
+    long findCeil(int mid,int piles[]){
+         long sum = 0;
+        // float res = 0;
+        for(int i = 0;i<piles.length;i++){
+            // res = (float)Math.ceil(piles[i]/(float)mid);
+            sum += (piles[i] + mid - 1L) / mid;
         }
-        return m;
-    }
-    public static int func(int k,int arr[])
-    {
-        int total=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            total+=Math.ceil((double)arr[i]/(double)k);
-        }
-        return total;
+        return sum;
     }
     public int minEatingSpeed(int[] piles, int h) {
-        int low=1;
-        int high=findmax(piles);
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            int f=func(mid,piles);
-            if(f<=h)
-            {
-                high=mid-1;
-            }else
-            {
-                low=mid+1;
+        int n = piles.length;
+        int maxi = 0;
+        for (int pile : piles) {
+            maxi = Math.max(maxi, pile);
+        }
+        // int temp[] = new int[maxi];
+        
+        // for(int i = 1;i<=maxi;i++){
+        //     temp[i-1] =  i;
+        // }
+        int low = 1;
+        int high = maxi;
+        //int ans = maxi;
+        while(low<=high){
+            int mid = (low+high)/2;
+            long hrs = findCeil(mid,piles);
+            if(hrs<=h){
+               // ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
             }
         }
         return low;
