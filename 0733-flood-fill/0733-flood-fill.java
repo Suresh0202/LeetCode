4a19f4118ef0a5a8c7fa08aa9f03eb8ca8447cc8
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-09-08 00:41:12 +0530

    Time: 1 ms (45.63%), Space: 45 MB (50.62%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,24 +1,32 @@
 class Solution {
-    public static void dfs(int image[][],int sr,int sc,int nw,int old)
+    public void func(int image[][],int sr,int sc,int n,int m,int nw,int vis[][],int org)
     {
-        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]==nw)
+        if(sr<0 || sc<0 || sc>=m || sr>=n || vis[sr][sc] == -1 || image[sr][sc] != org )
         {
             return;
         }
-        if(image[sr][sc]==old)
-        {
-            image[sr][sc]=nw;
-                dfs(image,sr+1,sc,nw,old);
-                dfs(image,sr-1,sc,nw,old);
-                dfs(image,sr,sc+1,nw,old); 
-                dfs(image,sr,sc-1,nw,old);
-        }
-
+        vis[sr][sc]=-1;
+        func(image,sr+1,sc,n,m,nw,vis,org);
+        func(image,sr-1,sc,n,m,nw,vis,org);
+        func(image,sr,sc+1,n,m,nw,vis,org);            
+        func(image,sr,sc-1,n,m,nw,vis,org);
     }
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc]!=color)
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        // Code here
+        int n=image.length;
+        int m=image[0].length;
+        int vis[][]=new int[n][m];
+        int org=image[sr][sc];
+        func(image,sr,sc,n,m,newColor,vis,org);
+        for(int i=0;i<n;i++)
         {
-            dfs(image,sr,sc,color,image[sr][sc]);
+            for(int j=0;j<m;j++)
+            {
+                if(vis[i][j] == -1)
+                {
+                    image[i][j]=newColor;
+                }
+            }
         }
         return image;
     }
