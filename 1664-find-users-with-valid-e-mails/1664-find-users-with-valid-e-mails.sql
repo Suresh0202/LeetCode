0ce37dd9f7ae46221757e4454ea31a775788187d
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-09-08 12:53:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
@@ -1,8 +1,3 @@
 # Write your MySQL query statement below
-select user_id,name,mail
-from Users
--- where (mail like '%@leetcode.com') and ((mail like '%.' )or (mail like '%-') or (mail like '%_')) and (mail not like '.%' or mail not like '%#%')
-where mail
-REGEXP '^[a-zA-Z][A-Za-z0-9_.-]*@leetcode\\.com$'
-
--- mail='%.' or mail = '%_' or mail ='%-') or
+select * from Users
+where mail regexp '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$'
