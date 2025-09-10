c699510e53dfd99fff59469aa5cee63d26ffd905
Suresh0202 <141948578+Suresh0202@users.noreply.github.com>
2025-09-10 18:11:57 +0530

    Time: 6 ms (72.14%), Space: 45.1 MB (83.42%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,44 +1,48 @@
 class Solution {
-    public boolean canFinish(int num, int[][] pre) {
-        int indeg[]=new int[num];
-        List<List<Integer>>adj=new ArrayList<>();
-        for(int i=0;i<num;i++)
+    public boolean canFinish(int n, int[][] pri) {
+         ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
+        // int m=pri.length;
+        for(int i=0;i<n;i++)
         {
             adj.add(new ArrayList<>());
         }
-        for(int p[]:pre)
-        {
-            int ai=p[0];
-            int bi=p[1];
-            adj.get(bi).add(ai);
-            indeg[ai]++;
-        }
-        Queue<Integer>qu=new LinkedList<>();
-        for(int i=0;i<indeg.length;i++)
-        {
-            if(indeg[i]==0)
-            {
-                qu.add(i);
-            }
-        }
-        int x=0;
-        List<Integer>ans=new ArrayList<>();
-        while(!qu.isEmpty())
-        {
-            int node=qu.peek();
-            qu.remove();
-            x++;
-            for(int it:adj.get(node))
-            {
-                indeg[it]--;
-                if(indeg[it]==0)
-                {
-                    qu.add(it);
-                }
-            }
-            
-        }
-        System.out.println(ans);
-        return x==num;
+       for(int e[]:pri)
+       {
+           int u=e[0];
+           int v=e[1];
+        //   adj.get(u).add(v);
+          adj.get(v).add(u);
+       }
+       int indeg[]=new int[n];
+       for(int i=0;i<n;i++)
+       {
+           for(int it:adj.get(i))
+           {
+               indeg[it]++;
+           }
+       }
+       Queue<Integer>qu=new LinkedList<>();
+       for(int i=0;i<n;i++)
+       {
+           if(indeg[i] == 0)
+           {
+               qu.add(i);
+           }
+       }
+       ArrayList<Integer>ans=new ArrayList<>();
+       while(!qu.isEmpty())
+       {
+           int node=qu.poll();
+           ans.add(node);
+           for(int  it:adj.get(node))
+           {
+               indeg[it]--;
+               if(indeg[it] == 0)
+               {
+                   qu.add(it);
+               }
+           }
+       }
+       return ans.size()==n;
     }
 }
